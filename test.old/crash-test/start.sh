0f5d898efc80f18da4d72774b3c442be5ce04601
Stephane Alnet <stephane@shimaore.net>
2015-07-29 16:09:11 +0200

    Removed the old tests

--- test.old/crash-test/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-killall opensips
-
-(sleep 3 && nc -u localhost 5067 < packet.txt) &
-
-HERE="`pwd`"
-cat startup.txt - | { cd ../../../opensips && gdb --args /usr/sbin/opensips -D -E -f "$HERE/opensips.cfg"; }
